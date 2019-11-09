1e817ef2947f4922417a10143b83a53ed9d190c2
Yehuda Deutsch <yeh@uda.co.il>
2019-11-09 12:12:22 +0200

    Improve initial files

--- preamble.php
+++ preamble.php
@@ -3,15 +3,26 @@
 if (isset($_SERVER['HTTP_X_FORWARDED_FOR'])) {
     $forwarded_for = $_SERVER['HTTP_X_FORWARDED_FOR'];
     if ($forwarded_for) {
+        $origin_remote_addr = $_SERVER['REMOTE_ADDR'];
         $forwarded_for = explode(',', $forwarded_for);
-        $forwarded_for = end($forwarded_for);
-        $forwarded_for = trim($forwarded_for);
-        $_SERVER['REMOTE_ADDR'] = $forwarded_for;
+        $possible_address = $origin_remote_addr;
+        foreach ($forwarded_for as $address) {
+            $address = trim($address);
+            if (!$address) {
+                continue;
+            }
+            if (!filter_var($address, FILTER_VALIDATE_IP,
+                FILTER_FLAG_IPV4 | FILTER_FLAG_IPV6 | FILTER_FLAG_NO_PRIV_RANGE | FILTER_FLAG_NO_RES_RANGE)) {
+                $possible_address = $address;
+                break;
+            }
+        }
+
+        $_SERVER['REMOTE_ADDR'] = $possible_address;
+        $_SERVER['ORIGIN_REMOTE_ADDR'] = $origin_remote_addr;
     }
 }
 
-if (isset($_SERVER['HTTP_X_FORWARDED_PROTO'])) {
-    if ($_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https') {
-        $_SERVER['HTTPS'] = true;
-    }
+if (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https') {
+    $_SERVER['HTTPS'] = true;
 }
